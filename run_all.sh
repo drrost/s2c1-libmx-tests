d47cb64daab6a6d913d445a9a5744d91b8f7552a
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-01 16:37:58 +0300

    Change scripts location

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,7 @@
 # Check mandatory files/folders exist
 MANDATORY_FILES="src/ inc/ inc/libmx.h obj/ Makefile"
 PATH_TO_SRC='../src/'
-./check_files_exist.sh $PATH_TO_SRC "$MANDATORY_FILES"
+./scripts/check_files_exist.sh $PATH_TO_SRC "$MANDATORY_FILES"
 
 # Check if only available files/folders are in the project
 
