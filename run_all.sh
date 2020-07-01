1d74378b5337b2f286a82b25dc7247682948e06d
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-01 16:34:53 +0300

    Add basic scripts

---
+++ run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check mandatory files/folders exist
+MANDATORY_FILES="src/ inc/ inc/libmx.h obj/ Makefile"
+PATH_TO_SRC='../src/'
+./check_files_exist.sh $PATH_TO_SRC "$MANDATORY_FILES"
+
+# Check if only available files/folders are in the project
+
+# Check there are no unavailable functions used
+AVAILABLE_FUNCTIONS=(open write)
