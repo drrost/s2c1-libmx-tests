50a3c426e198ef3017b752ec60b31f4d56f7b395
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-09 18:25:22 +0300

    Add a test that does not work

---
+++ tests/t01/test_00.c
@@ -0,0 +1,15 @@
+
+#include "../../lib/test_lib.h"
+#include "../../libmx.h"
+
+// Tests
+
+void test_print_unicode_ok() {
+    // Given
+    w_char wc = 'a';
+
+    // When
+    mx_print_unicode(wc);
+
+    // Then
+}
