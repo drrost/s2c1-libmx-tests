a53dea97ed0f107fc1266cee10ae06674d3306fb
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-15 14:14:47 +0300

    Add tests for strcpy and strncpy

---
+++ tests/t22/test_00.c
@@ -0,0 +1,18 @@
+
+#include "../../lib/test_lib.h"
+#include "../../libmx.h"
+
+// Tests
+
+void test_strcpy() {
+    // Given
+    char s1[] = "abc";
+    char s2[] = "000";
+
+    // When
+    char *s3 = mx_strcpy(s2, s1);
+
+    // Then
+    ASSERT_EQUALS(strcmp(s2, s1), 0);
+    ASSERT_EQUALS(s2, s3);
+}
---
+++ tests/t23/test_00.c
@@ -0,0 +1,44 @@
+
+#include "../../lib/test_lib.h"
+#include "../../libmx.h"
+
+// Tests
+
+void test_strncpy() {
+    // Given
+    char s1[] = "abc";
+    char s2[] = "000";
+
+    // When
+    char *s3 = mx_strncpy(s2, s1, 3);
+
+    // Then
+    ASSERT_EQUALS(strcmp(s2, s1), 0);
+    ASSERT_EQUALS(s2, s3);
+}
+
+void test_strncpy_2() {
+    // Given
+    char s1[] = "abc";
+    char s2[] = "000";
+
+    // When
+    char *s3 = mx_strncpy(s2, s1, 2);
+
+    // Then
+    ASSERT_EQUALS(strcmp(s2, "ab0"), 0);
+    ASSERT_EQUALS(s2, s3);
+}
+
+void test_strncpy_zero() {
+    // Given
+    char s1[] = "abc";
+    char s2[] = "000";
+
+    // When
+    char *s3 = mx_strncpy(s2, s1, 0);
+
+    // Then
+    ASSERT_EQUALS(strcmp(s2, "000"), 0);
+    ASSERT_EQUALS(s2, s3);
+}
