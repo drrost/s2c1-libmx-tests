c6d7141d91275889383d46e3f2a0da228684c486
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-15 13:27:36 +0300

    Add tests for strndup

--- tests/t21/test_00.c
+++ tests/t21/test_00.c
@@ -12,6 +12,45 @@ void test_strndup() {
     char *s2 = mx_strndup(s1, 3);
 
     // Then
+    ASSERT_TRUE(strcmp("Hel", s2) == 0);
+    ASSERT_TRUE(s1 != s2);
+    free(s2);
+}
+
+void test_strndup_zero() {
+    // Given
+    char *s1 = "Hello, world!";
+
+    // When
+    char *s2 = mx_strndup(s1, 0);
+
+    // Then
+    ASSERT_TRUE(strcmp("", s2) == 0);
+    ASSERT_TRUE(s1 != s2);
+    free(s2);
+}
+
+void test_strndup_minus() {
+    // Given
+    char *s1 = "Hello, world!";
+
+    // When
+    char *s2 = mx_strndup(s1, -1);
+
+    // Then
+    ASSERT_TRUE(strcmp(s1, s2) == 0);
+    ASSERT_TRUE(s1 != s2);
+    free(s2);
+}
+
+void test_strndup_greater_than_len() {
+    // Given
+    char *s1 = "Hello, world!";
+
+    // When
+    char *s2 = mx_strndup(s1, 1024);
+
+    // Then
     ASSERT_TRUE(strcmp(s1, s2) == 0);
     ASSERT_TRUE(s1 != s2);
     free(s2);
