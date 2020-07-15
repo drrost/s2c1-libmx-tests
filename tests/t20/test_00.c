8e6e8bdf3a6f04288bb49ac2ea5548858f32ff7c
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-15 13:29:05 +0300

    Add one more test for strdup

--- tests/t20/test_00.c
+++ tests/t20/test_00.c
@@ -16,3 +16,16 @@ void test_strdup() {
     ASSERT_TRUE(s1 != s2);
     free(s2);
 }
+
+void test_strdup_empty() {
+    // Given
+    char *s1 = "";
+
+    // When
+    char *s2 = mx_strdup(s1);
+
+    // Then
+    ASSERT_TRUE(strcmp(s1, s2) == 0);
+    ASSERT_TRUE(s1 != s2);
+    free(s2);
+}
