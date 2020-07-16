dea67aad0234686eb4bf511e476f535b5610e02c
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-07-16 11:33:55 +0300

    Add tests for several functions

--- tests/t34/test_00.c
+++ tests/t34/test_00.c
@@ -41,7 +41,9 @@ void test_frist_0() {
     char *result = mx_strjoin(str1, str2);
 
     // Then
-    ASSERT_TRUE(result == 0);
+    ASSERT_TRUE(strcmp(str2, result) == 0);
+    ASSERT_TRUE(str2 != result);
+
 
     free(result);
 }
@@ -55,7 +57,8 @@ void test_second_0() {
     char *result = mx_strjoin(str1, str2);
 
     // Then
-    ASSERT_TRUE(result == 0);
+    ASSERT_TRUE(strcmp(str1, result) == 0);
+    ASSERT_TRUE(str1 != result);
 
     free(result);
 }
@@ -70,6 +73,63 @@ void test_both_0() {
 
     // Then
     ASSERT_TRUE(result == 0);
+
+    free(result);
+}
+
+void test_strjoin_ex1() {
+    // Given
+    char str1[] = "this";
+    char str2[] = "dodge ";
+
+    // When
+    char *result = mx_strjoin(str2, str1);
+
+    // Then
+    ASSERT_TRUE(strcmp("dodge this", result) == 0);
+
+    free(result);
+}
+
+void test_strjoin_ex2() {
+    // Given
+    char str1[] = "this";
+    char *str3 = 0;
+
+    // When
+    char *result = mx_strjoin(str1, str3);
+
+    // Then
+    ASSERT_TRUE(strcmp("this", result) == 0);
+
+    free(result);
+}
+
+void test_strjoin_ex2_1() {
+    // Given
+    char *str1 = 0;
+    char str2[] = "dodge ";
+
+    // When
+    char *result = mx_strjoin(str1, str2);
+
+    // Then
+    ASSERT_TRUE(strcmp(str2, result) == 0);
+    ASSERT_TRUE(str2 != result);
+
+    free(result);
+}
+
+void test_strjoin_ex3() {
+    // Given
+    char *str1 = 0;
+    char *str2 = 0;
+
+    // When
+    char *result = mx_strjoin(str1, str2);
+
+    // Then
+    ASSERT_NULL(result);
 
     free(result);
 }
