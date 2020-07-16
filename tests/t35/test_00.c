87b3c6928590982ecd802ee2c44427b5d43fcc3a
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-07-16 11:43:16 +0300

    Add a test set

---
+++ tests/t35/test_00.c
@@ -0,0 +1,47 @@
+
+#include "../../lib/test_lib.h"
+#include "../../libmx.h"
+
+// Tests
+
+void test_mx_file_to_str() {
+    // Given
+    const char *file_name = "matrix.txt";
+
+    // When
+    char *result = mx_file_to_str(file_name);
+
+    // Then
+    ASSERT_TRUE(strcmp(result, "Matrix") == 0);
+    ASSERT_EQUALS(6, strlen(result));
+
+    free(result);
+}
+
+void test_mx_file_to_str_2() {
+    // Given
+    const char *file_name = "matrix2.txt";
+
+    // When
+    char *result = mx_file_to_str(file_name);
+
+    // Then
+//    printf("result: \"%s\"\n", result);
+    ASSERT_TRUE(strcmp(result, "Matrix\nMatrix") == 0);
+    ASSERT_EQUALS(13, strlen(result));
+
+    free(result);
+}
+
+void test_mx_file_to_str_unexists() {
+    // Given
+    const char *file_name = "absent.txt";
+
+    // When
+    char *result = mx_file_to_str(file_name);
+
+    // Then
+    ASSERT_EQUALS(0, result);
+
+    free(result);
+}
