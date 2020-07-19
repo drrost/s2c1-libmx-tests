0fc906cb643e3f115b9698f1846f9d1402660adc
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-07-19 19:20:30 +0300

    Add a tests set

--- tests/t45/test_00.c
+++ tests/t45/test_00.c
@@ -8,7 +8,8 @@ void test_a() {
     // https://riptutorial.com/c/example/2150/copying-overlapping-memory
     // https://www.student.cs.uwaterloo.ca/~cs350/common/os161-src-html/doxygen/html/memmove_8c_source.html
     char str[19] = "This is an example";
-    memcpy(str + 7, str, 10);
-
-    ASSERT_TRUE(0);
+    char *result = mx_memmove(str + 7, str, 10);
+    puts(str);
+    ASSERT_EQUALS_STR("This isThis is ane", str);
+    ASSERT_EQUALS(str + 7, result);
 }
