39c389cd7b2ba2ea0197e7ea4099fe64f188a7ba
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-21 10:39:34 +0300

    Add tests for quicksort

--- tests/t13/test_00.c
+++ tests/t13/test_00.c
@@ -24,4 +24,27 @@ void test_quick_sort_arr_example_0() {
 
     // Then
     ASSERT_EQUALS(2, result);
+
+    ASSERT_EQUALS_STR("Raphael", arr[0]);
+    ASSERT_EQUALS_STR("Leonardo", arr[1]);
+    ASSERT_EQUALS_STR("Donatello", arr[2]);
+    ASSERT_EQUALS_STR("Michelangelo", arr[3]);
+}
+
+void test_quick_sort_arr_example_1() {
+    // Given
+    char *arr[] = {"DMC", "Clint Eastwood", "Dr Brown", "Einstein", "Jessica", "Biff Tannen"};
+
+    // When
+    int result = mx_quicksort(arr, 0, 5);
+
+    // Then
+    ASSERT_EQUALS(2, result);
+
+    ASSERT_EQUALS_STR("DMC", arr[0]);
+    ASSERT_EQUALS_STR("Jessica", arr[1]);
+    ASSERT_EQUALS_STR("Dr Brown", arr[2]);
+    ASSERT_EQUALS_STR("Einstein", arr[3]);
+    ASSERT_EQUALS_STR("Biff Tannen", arr[4]);
+    ASSERT_EQUALS_STR("Clint Eastwood", arr[5]);
 }
