c14c354100500224ef0b1580cbddec3b6edc445f
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-07-25 21:34:14 +0300

    Move try/catch to test_lib

--- tests/t03/test_00.c
+++ tests/t03/test_00.c
@@ -4,44 +4,7 @@
 
 char *test_case_name = "mx_print_strarr";
 
-// SEGFAULT interception
-#include <setjmp.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-
-static sigjmp_buf point;
-
-static void handler(int sig, siginfo_t *siginfo, void *p) {
-    sig++;
-    siginfo++;
-    char *a = (char *) p;
-    a++;
-    longjmp(point, 1);
-}
-
 // Tests
-
-#define TRY \
-    struct sigaction sa; \
-    memset(&sa, 0, sizeof(struct sigaction)); \
-    sigemptyset(&sa.sa_mask); \
-\
-    sa.sa_flags = SA_NODEFER; \
-    sa.sa_sigaction = handler; \
-\
-    sigaction(SIGSEGV, &sa, NULL); \
-    if (setjmp(point) == 0) { \
-
-#define CATCH \
-    } else { \
-    restore_stdout(); \
-    char *s = (char *) malloc(1024); \
-    sprintf(s, "     Segmentation fault: \"\033[1m%s()\033[0m\"\n", __func__); \
-    test_print_fail(s); \
-    free(s); \
-    return; \
-}
 
 void test_print_strarr_basic() {
     // Given
